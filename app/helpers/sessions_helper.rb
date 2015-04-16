1df827fb9972f1820304f0545c2da235a6aa148d
limkimhuor <kimhuorlim@gmail.com>
2015-04-16 09:43:53 +0700

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,7 @@ module SessionsHelper
 			@current_user ||= User.find_by(id: session[:user_id])
 		    elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
+			if user && user.authenticated?(:remember, cookies[:remember_token])
 				log_in user
 				@current_user = user
 			end
@@ -58,6 +58,5 @@ module SessionsHelper
 	def store_location
 		session[:forwarding_url] = request.url if request.get?
 	end
-
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,6 +59,4 @@ class UsersControllerTest < ActionController::TestCase
   end
 
 
-
-
 end
